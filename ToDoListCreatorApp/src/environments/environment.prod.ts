6cd8b65ee840dbc00b4ccfb1bdf8f472560e066f
jennyhalo <jenny.halonen@student.laurea.fi>
2022-12-04 16:17:56 +0200

    some changes

--- ToDoListCreatorApp/src/environments/environment.prod.ts
+++ ToDoListCreatorApp/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyA1C64wmfUo5W1BgzRSL4tB8zYSjBZCN2I",
+    authDomain: "todolistcreatorapp.firebaseapp.com",
+    projectId: "todolistcreatorapp",
+    storageBucket: "todolistcreatorapp.appspot.com",
+    messagingSenderId: "318290465869",
+    appId: "1:318290465869:web:9a5415b443fc12c0fcb804"
+  }
 };
