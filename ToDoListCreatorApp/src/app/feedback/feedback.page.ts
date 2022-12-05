6ddc48d96765b235d5ce032efb54003bd7be9a4d
jennyhalo <jenny.halonen@student.laurea.fi>
2022-12-05 22:52:47 +0200

    updated thank you and feedback page

--- ToDoListCreatorApp/src/app/feedback/feedback.page.ts
+++ ToDoListCreatorApp/src/app/feedback/feedback.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-feedback',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedbackPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
+  sendFeedback() {
 
+    this.router.navigate(['/thank-you']);
+
+  }
 }
