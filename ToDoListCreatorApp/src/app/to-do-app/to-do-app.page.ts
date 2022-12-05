ba69f7a65f38fa092b09442a80a6b5963a42d8f7
Oonuliina <91269427+Oonuliina@users.noreply.github.com>
2022-12-06 00:50:10 +0200

    to-do-list scetch

--- ToDoListCreatorApp/src/app/to-do-app/to-do-app.page.ts
+++ ToDoListCreatorApp/src/app/to-do-app/to-do-app.page.ts
@@ -7,6 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToDoAppPage implements OnInit {
 
+  todoList = [{
+    itemName : 'Coding',
+    itemDueDate: '13-10-21',
+    itemPriority: 'high',
+    itemCategory: 'Work' 
+  },
+  {
+    itemName : 'Coding',
+    itemDueDate: '13-10-21',
+    itemPriority: 'low',
+    itemCategory: 'Word' 
+  },
+  {
+    itemName : 'Workout',
+    itemDueDate: '13-05-22',
+    itemPriority: 'high',
+    itemCategory: 'Personal' 
+  }
+]
+
+  today : number = Date.now()
+
   constructor() { }
 
   ngOnInit() {
